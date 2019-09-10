4f27513320a2666d19a17fcee2c26274c580929c
JIANCPP1 <55116054+JIANCPP1@users.noreply.github.com>
2019-09-09 21:51:38 -0300

    Calculo ainda em desenvolvimento

---
+++ calculadora.java
@@ -0,0 +1,66 @@
+/**
+	Classe que simula cálculos aritméticos entre dois números inteiros.
+	@autor Jian Carlos Procópio de Paula
+	@since 09-09-2019
+*/	
+	
+public class calculadora {
+	
+	int numero1, numero2;
+	
+	int obterResto (int x, int y)  {
+	int resto;
+	resto = (x%y);
+
+		return resto;
+		
+	}
+	
+	void imprimiRestos (int[] vetx, int[] vety, int formato) {
+		
+		calculadora.
+		
+		
+		
+	
+	public static void main(String[] args) {
+		
+		calculadora cal = new calculadora();
+		cal.numero1 = 10;
+		cal.numero2 = 3;
+		int resto = cal.obterResto(cal.numero1,cal.numero2);
+		System.out.print(String.format("o resto da divisão de %d por %d é %d", cal.numero1, cal.numero2, resto)); }
+
+		imprimiRestos vetx = new imprimiRestos ();
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+}	
